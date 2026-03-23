8a6ceaafe46812c68908588318217c1101987a3a
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-03-23 19:57:30 +0300

    Initial Commit

---
+++ module4lesson1/module4lesson1App.swift
@@ -0,0 +1,17 @@
+//
+//  module4lesson1App.swift
+//  module4lesson1
+//
+//  Created by Максим Минаков on 23.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct module4lesson1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
